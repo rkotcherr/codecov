04642bb6080f88219a3acf2413b076d571fdc045
rkotcher <rkotcher@gmail.com>
2020-03-28 11:55:12 -0400

    This commit should increase branch coverage by 50%

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,4 +5,10 @@ describe('Babel usage suite', () => {
   it('should return empty array for empty array', () => {
     assert(getOdd([]), [])
   });
+
+  it('should return [1] for [1]', () => {
+    const odd = getOdd([1]);
+    assert(odd.length, 1);
+    assert(odd[0], 1);
+  });
 });
